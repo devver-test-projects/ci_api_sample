0e8d5609abc1f780815c51fe2dbd8b1cdb4e2d7b
Dan Mayer <danmayer@dan-mayers-macbook.local>
2009-09-15 16:19:01 -0400

    first commit

---
+++ test/unit/batman_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class BatmanTest < ActiveSupport::TestCase
+  # Replace this with your real tests.
+  test "the truth" do
+    assert true
+  end
+
+  test "im batman" do
+    assert_equal "I'm batman", "I'm batman"
+  end
+
+  test "im the mf batman" do
+    assert_equal "I'm the mf batman", "I'm the mf batman"
+  end
+
+ 
+end
