a44c4ed8fb104664b251b4c95ef1c3b57f9fb462
Avdi Grimm <avdi@avdi.org>
2009-11-11 17:13:00 -0500

    Added a method for metric comparison troubleshooting

--- app/models/batman.rb
+++ app/models/batman.rb
@@ -1,2 +1,13 @@
 class Batman < ActiveRecord::Base
+  def complex_method(x)
+    case x
+    when nil then puts "nil"
+    when Numeric
+      if x == 3
+        puts "you win!"
+      else
+        puts "you still win!"
+      end
+    end
+  end
 end
